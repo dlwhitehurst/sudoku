75dca62c9cae0c3dc7784a33d92b5ded27f4cd0a
David Whitehurst <>
2019-11-08 11:50:23 -0500

    added copyright for apache 2.0 all files

--- src/main/java/com/dlw/sudoku/types/Grid.java
+++ src/main/java/com/dlw/sudoku/types/Grid.java
@@ -1,16 +1,48 @@
+/*
+ * Copyright (c) 2019. David L. Whitehurst
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dlw.sudoku.types;
 
 public class Grid {
 
+    /**
+     * The data structure
+     */
     protected char[][] theGrid;
+
+    /**
+     * Depth of grid in rows
+     */
     protected int rows;
+
+    /**
+     * Width of grid in rows
+     */
     protected int cols;
 
 
     /**
-     * Class constructor
+     * Default Class constructor
      */
     public Grid() {} // default
+
+    /**
+     * Class constructor with sizing arguments
+     * @param rows
+     * @param cols
+     */
     public Grid(int rows,int cols) {
         theGrid = new char[rows][cols];
         this.rows = rows;
